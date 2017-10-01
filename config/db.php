cb4770768977791d6f13b7c2958ba9a5139ef232
Robert Gard <robert@Robert.Robert-pc>
2017-10-01 22:07:02 +0300

    Создал миграции. С помощью GII создал CRUD.

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=maindb',
+    'username' => 'root',
+    'password' => '1997',
+    'charset' => 'utf8',
+];
